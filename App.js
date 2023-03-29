03e1bd299cef3bc10cca67c3446ce655a629cc4d
mohab9248 <mohamad.abbas9248@gmail.com>
2023-03-29 11:13:21 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(){
+
+  return (
+  <View>
+    <Text>
+      hello 
+    </Text>
+  </View>
+  );
+}
+
+
+export default App;
