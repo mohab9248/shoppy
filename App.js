e9f20d6dbb634a830fc5a56cbac13abd7f0ef838
mohab9248 <mohamad.abbas9248@gmail.com>
2023-06-13 13:57:38 +0300

    app icon

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import NavBar from './src/navigators/NavBar';
+import {enableLatestRenderer} from 'react-native-maps';
+
+enableLatestRenderer();
 const App = () => {
   return (
     <NavigationContainer>
